ccaf1e022eab11a48537ecf35f3216034e472de7
Lujiabz <lujiabz@gmail.com>
2018-10-08 16:57:36 +0800

    feat(webpack config): init webpack config

---
+++ src/main.js
@@ -0,0 +1,3 @@
+require("@/css/main.css")
+console.log(1);
+console.log(1244);
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const webpack = require('webpack');
+function resolve (dir) {
+  return path.join(__dirname, dir)
+}
+const config = {
+  entry: path.resolve(__dirname, 'src/main.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: "[name].js"
+  },
+  resolve: {
+    // extensions: ['.js', '.vue', '.json'],
+    alias: {
+      '@': resolve('src'),
+    }
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, ''),
+    port: 9000,
+    hot: true,
+    clientLogLevel: 'none',
+    compress: true
+  },
+  module: {
+    rules: [
+      { 
+        test: /\.css$/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader" }
+        ]
+      },
+      { test: /\.ts$/, use: 'ts-loader' }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  mode: 'development'
+};
+
+module.exports = config;
